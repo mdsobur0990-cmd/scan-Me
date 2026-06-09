0f12ba759117681f7b18d720c9ef217b2946f255
mdsobur0990-cmd <mdsobur0990@gmail.com>
2026-06-09 21:00:17 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,6 @@
-// sw.js - The Service Worker
-const CACHE_NAME = 'pro-scanner-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
-  'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet',
-  'https://cdn.jsdelivr.net/npm/@tensorflow-models/knn-classifier'
-];
-
-// Install & Cache Assets
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open('v1').then(cache => cache.addAll(['index.html'])));
 });
-
-// Serve from Cache when offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
